3a4cf44f0d66c3229ea6fcd0f6f0653fd4498d54
Jeremy Williamson <jeremy.williamson83@gmail.com>
2021-09-28 22:47:06 -0400

    build and debug in one key press

--- src/Hello_World.adb
+++ src/Hello_World.adb
@@ -3,4 +3,5 @@ with Ada.Text_IO;
 procedure Hello_World is
 begin
     Ada.Text_IO.Put_Line ("Hello Ada World!");
+    Ada.Text_IO.Put_Line("another line to test build/break with launch.json");
 end Hello_World;
